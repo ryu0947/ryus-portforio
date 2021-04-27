affc2aba8e64ee80936d1b9680b58d92f51b93bd
ryu <dragonogawaz@gmail.com>
2021-04-27 20:45:41 +0900

    ページ内リンクの処理実装

---
+++ js/page-link.js
@@ -0,0 +1,20 @@
+"use strict";
+{
+  const pageLink = document.querySelectorAll("a[href^='#']");
+
+  pageLink.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = a.getAttribute("href");
+      const getTargetId = document.getElementById(href.replace("#", ""));
+      const getElementPosition = getTargetId.getBoundingClientRect().top;
+      const calcElementPosition =
+        getElementPosition + window.pageYOffset;
+      window.scrollTo({
+        top: calcElementPosition,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  });
+}
