d8fe5f6188f7e9f99575b495ede6fa1f2944bd25
ryu <dragonogawaz@gmail.com>
2021-04-27 19:08:57 +0900

    ヘッダーを追従する処理を実装

--- js/header.js
+++ js/header.js
@@ -1,6 +1,19 @@
 "use strict";
 
 {
- 
+  const main = document.getElementById("js-main");
+  const header = document.getElementById("js-header");
+  const headerHeight = header.offsetHeight;
+  const fv = document.getElementById("js-fv");
+  const fvHeight = fv.offsetHeight;
 
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > fvHeight) {
+      header.classList.add("fixed");
+      main.style.marginTop = `${headerHeight}px`;
+    } else {
+      header.classList.remove("fixed");
+      main.style.marginTop = "";
+    }
+  });
 }
