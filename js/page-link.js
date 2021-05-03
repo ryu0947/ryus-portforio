1b8a168649142446bb108af90db162b1bbeb026f
ryu <dragonogawaz@gmail.com>
2021-05-03 17:59:16 +0900

    変数名を変更

--- js/page-link.js
+++ js/page-link.js
@@ -1,18 +1,15 @@
 "use strict";
 
 {
-  import smoothscroll from "smoothscroll-polyfill";
-  smoothscroll.polyfill();
-
   const pageLink = document.querySelectorAll("a[href^='#']");
 
   pageLink.forEach((a) => {
     a.addEventListener("click", (e) => {
       e.preventDefault();
       const href = a.getAttribute("href");
-      const getTargetId = document.getElementById(href.replace("#", ""));
-      const getElementPosition = getTargetId.getBoundingClientRect().top;
-      const calcElementPosition = getElementPosition + window.pageYOffset;
+      const targetId = document.getElementById(href.substr(1));
+      const elementPosition = targetId.getBoundingClientRect().top;
+      const calcElementPosition = elementPosition + window.pageYOffset;
       window.scrollTo({
         top: calcElementPosition,
         left: 0,
