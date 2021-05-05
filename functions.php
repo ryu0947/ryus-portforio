bc9a376e29b65b6a6ac471f5400f9c8685f99715
ryu <dragonogawaz@gmail.com>
2021-05-06 03:11:38 +0900

    jsファイル追加

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function add_file()
     wp_enqueue_style('all', get_template_directory_uri() . '/css/all.css');
     wp_enqueue_style('fontawsome', get_template_directory_uri() . '/css/fontawsome.css');
     if (is_front_page()) {
+        wp_enqueue_script('smooth', get_template_directory_uri() . '/js/smoothscroll.js', array(), null, true);
         wp_enqueue_script('page-link', get_template_directory_uri() . '/js/page-link.js', array(), null, true);
         wp_enqueue_style('styles', get_template_directory_uri() . '/css/styles.css');
     } else {
@@ -12,6 +13,7 @@ function add_file()
     }
 }
 
+// CSSとJS ファイルを追加
 add_action('wp_enqueue_scripts', 'add_file');
 
 // pタグの自動挿入を無効化する
